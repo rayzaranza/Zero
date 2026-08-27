0a83c7ba87ab81deec3c68259f7233f5d7bde1b6
Ray Zaranza <rayzaranza@gmail.com>
2026-08-27 18:35:39 -0300

    refactor: change default log level to info

--- src/Logger/Logger.h
+++ src/Logger/Logger.h
@@ -13,8 +13,8 @@ namespace ZY
 	};
 }
 
-#define LOG(...) ::ZY::Logger::logger->trace(__VA_ARGS__)
-#define LOG_INFO(...) ::ZY::Logger::logger->info(__VA_ARGS__)
+#define LOG(...) ::ZY::Logger::logger->info(__VA_ARGS__)
+#define LOG_TRACE(...) ::ZY::Logger::logger->trace(__VA_ARGS__)
 #define LOG_ERROR(...) ::ZY::Logger::logger->error(__VA_ARGS__)
 #define LOG_WARN(...) ::ZY::Logger::logger->warn(__VA_ARGS__)
 #define LOG_CRITICAL(...) ::ZY::Logger::logger->critical(__VA_ARGS__)
