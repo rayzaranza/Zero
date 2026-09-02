45e1977dbfdbf60849c7be6c9a92caa3eef92f1b
Ray Zaranza <rayzaranza@gmail.com>
2026-09-02 18:29:29 -0300

    feat: add layer stack to application

--- Zero/src/Application/Application.cpp
+++ Zero/src/Application/Application.cpp
@@ -16,13 +16,35 @@ namespace Zero
         EventDispatcher dispatcher { event };
         dispatcher.Dispatch<WindowClosedEvent>(std::bind(&Application::onWindowClosed, this, std::placeholders::_1));
 
-        ZERO_CORE_LOG(event);
+        for (LayerPointerArray::iterator iterator { m_LayerStack.end() }; iterator != m_LayerStack.begin();)
+        {
+            (*--iterator)->OnEvent(event);
+            if (event.IsHandled())
+            {
+                break;
+            }
+        }
+    }
+
+    void Application::PushLayer(Layer* layer)
+    {
+        m_LayerStack.PushLayer(layer);
+    }
+
+    void Application::PushOverlay(Layer* overlay)
+    {
+        m_LayerStack.PushOverlay(overlay);
     }
 
     void Application::Run()
     {
         while (m_IsRunning)
         {
+            for (Layer* layer : m_LayerStack)
+            {
+                layer->OnUpdate();
+            }
+
             m_Window->OnUpdate();
         }
     }
