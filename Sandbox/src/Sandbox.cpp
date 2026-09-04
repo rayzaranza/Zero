e3d57cec1a670ce5c1624ac5e9b7dd8814c1a1ef
Ray Zaranza <rayzaranza@gmail.com>
2026-09-04 12:26:45 -0300

    feat: add key pressed event test in sandbox

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -9,12 +9,25 @@ class ExampleLayer : public Zero::Layer
   public:
     void OnUpdate() override
     {
-        ZERO_INFO("ExampleLayer OnUpdate");
+        if (Zero::Input::IsKeyPressed(ZERO_KEY_TAB))
+        {
+            ZERO_INFO("Tab key is pressed");
+        }
     }
 
     void OnEvent(Zero::Event& event) override
     {
-        ZERO_LOG("{0}", event);
+        if (event.GetEventType() == Zero::EventType::KeyPressed)
+        {
+            Zero::KeyPressedEvent& e { (Zero::KeyPressedEvent&)event };
+
+            if (e.GetKeyCode() == ZERO_KEY_A)
+            {
+                ZERO_INFO("A key is pressed");
+            }
+
+            ZERO_LOG("{0}", (char)e.GetKeyCode());
+        }
     }
 };
 
