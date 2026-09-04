d5e9e8a5a84e2fe6fc8ea2e7bfa2602b7f0761b1
Ray Zaranza <rayzaranza@gmail.com>
2026-09-04 12:25:55 -0300

    refactor: update logger formatting

--- Zero/src/Logger/Logger.cpp
+++ Zero/src/Logger/Logger.cpp
@@ -8,12 +8,12 @@ namespace Zero
 
     void Logger::Init()
     {
-        spdlog::set_pattern("%^[%T] %n: %v%$");
+        spdlog::set_pattern("%^%n | %T | %v%$");
 
-        s_CoreLogger = spdlog::stdout_color_mt("Zero");
+        s_CoreLogger = spdlog::stdout_color_mt("ZERO");
         s_CoreLogger->set_level(spdlog::level::trace);
 
-        s_ClientLogger = spdlog::stdout_color_mt("Client");
+        s_ClientLogger = spdlog::stdout_color_mt("APP ");
         s_ClientLogger->set_level(spdlog::level::trace);
     }
 
