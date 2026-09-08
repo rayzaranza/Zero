a8bc7ade02547bd6fa8bea9a71ee553c8f10128f
Ray Zaranza <rayzaranza@gmail.com>
2026-09-08 00:03:17 -0300

    feat: handle camera in renderer

--- Zero/src/Zero/Renderer/Renderer.cpp
+++ Zero/src/Zero/Renderer/Renderer.cpp
@@ -2,11 +2,17 @@
 
 namespace Zero
 {
-    void Renderer::BeginScene(const Camera& camera)
-    {}
+    Renderer::SceneData* Renderer::s_SceneData { new Renderer::SceneData() };
 
-    void Renderer::Submit(const std::shared_ptr<VertexArray>& vertexArray)
+    void Renderer::BeginScene(CameraOrthographic& camera)
     {
+        s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
+    }
+
+    void Renderer::Submit(const std::shared_ptr<VertexArray>& vertexArray, const std::shared_ptr<Shader>& shader)
+    {
+        shader->Bind();
+        shader->SetUniform("u_ViewProjectionMatrix", s_SceneData->ViewProjectionMatrix);
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
     }
--- Zero/src/Zero/Renderer/Renderer.h
+++ Zero/src/Zero/Renderer/Renderer.h
@@ -3,17 +3,26 @@
 #include "Zero/Camera/Camera.h"
 #include "Zero/Renderer/RenderCommand.h"
 #include "Zero/Renderer/RendererAPI.h"
+#include "Zero/Renderer/Shader.h"
 
 namespace Zero
 {
     class Renderer
     {
       public:
-        static void BeginScene(const Camera& camera);
+        static void BeginScene(CameraOrthographic& camera);
+        static void Submit(const std::shared_ptr<VertexArray>& vertexArray, const std::shared_ptr<Shader>& shader);
         static void EndScene();
-        static void Submit(const std::shared_ptr<VertexArray>& vertexArray);
 
       public:
         inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
+
+      private:
+        struct SceneData
+        {
+            glm::mat4 ViewProjectionMatrix { 1.0f };
+        };
+
+        static SceneData* s_SceneData;
     };
 }
