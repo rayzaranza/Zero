ab884353278c58d14056fa46c7b15ecddf9d6a55
Ray Zaranza <rayzaranza@gmail.com>
2026-09-13 13:27:43 -0300

    refactor: convert rendererAPI to scope

--- Zero/src/Zero/Renderer/RendererAPI.cpp
+++ Zero/src/Zero/Renderer/RendererAPI.cpp
@@ -1,6 +1,21 @@
 #include "Zero/Renderer/RendererAPI.h"
 
+#include "Zero/Renderer/OpenGL/OpenGLRendererAPI.h";
+
 namespace Zero
 {
     RendererAPI::API RendererAPI::s_API{ RendererAPI::API::OpenGL };
+
+    Scope<RendererAPI> RendererAPI::Create()
+    {
+        switch (s_API)
+        {
+            case API::None:   ZR_CORE_ASSERT(false, "Renderer API None is not supported"); return nullptr;
+            case API::Vulkan: ZR_CORE_ASSERT(false, "Renderer API Vulkan is not supported"); return nullptr;
+            case API::OpenGL: return CreateScope<OpenGLRendererAPI>();
+        }
+
+        ZR_CORE_ASSERT(false, "Unknown Renderer API");
+        return nullptr;
+    }
 }
