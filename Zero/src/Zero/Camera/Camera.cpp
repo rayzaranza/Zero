64434c76c7e36f574c557c59f4d66d277f9b42f8
Ray Zaranza <rayzaranza@gmail.com>
2026-09-13 13:45:10 -0300

    refactor: move matrix calculation to camera initializers list

--- Zero/src/Zero/Camera/Camera.cpp
+++ Zero/src/Zero/Camera/Camera.cpp
@@ -9,6 +9,7 @@ namespace Zero
         , m_ViewMatrix{ 1.0f }
         , m_ProjectionMatrix{ glm::ortho(left, right, bottom, top, -1.0f, 1.0f) }
         , m_Rotation{ 0.0f }
+        , m_ViewProjectionMatrix{ m_ProjectionMatrix * m_ViewMatrix }
     {
         ZR_PROFILE_FUNCTION();
     }
