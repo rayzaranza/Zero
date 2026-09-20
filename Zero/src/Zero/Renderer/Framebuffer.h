0efd2c2a65c734427c2e106fda03da00daa76024
Ray Zaranza <rayzaranza@gmail.com>
2026-09-20 14:41:29 -0300

    feat: add virtual destructor to framebuffer

--- Zero/src/Zero/Renderer/Framebuffer.h
+++ Zero/src/Zero/Renderer/Framebuffer.h
@@ -2,19 +2,22 @@
 #include "Zero/Core/Core.h"
 
 namespace Zero {
-  struct FramebufferProps {
-    glm::uvec2 Size{ 1280u, 720u };
-    uint32_t Samples{ 1u };
-    bool SwapChainTarget{ false };
-  };
 
-  class Framebuffer {
-  public:
-    virtual void Bind() const = 0;
-    virtual void Unbind() const = 0;
-    virtual void Resize(const glm::vec2& size) = 0;
-    virtual uint32_t GetColorAttachmentRendererID() const = 0;
-    virtual const FramebufferProps& GetProps() const = 0;
-    static Ref<Framebuffer> Create(const FramebufferProps& props);
-  };
+struct FramebufferProps {
+  glm::uvec2 Size{ 1280u, 720u };
+  uint32_t Samples{ 1u };
+  bool SwapChainTarget{ false };
+};
+
+class Framebuffer {
+public:
+  virtual ~Framebuffer() = default;
+  virtual void Bind() const = 0;
+  virtual void Unbind() const = 0;
+  virtual void Resize(const glm::vec2& size) = 0;
+  virtual uint32_t GetColorAttachmentRendererID() const = 0;
+  virtual const FramebufferProps& GetProps() const = 0;
+  static Ref<Framebuffer> Create(const FramebufferProps& props);
+};
+
 }
